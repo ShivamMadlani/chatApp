99b76cf89795dfc169425a6a55fe22d521e478b1
Shivam <shivammadlani5@gmail.com>
2022-11-16 14:42:14 +0530

    update

--- app.js
+++ app.js
@@ -2,23 +2,24 @@
 // const mongoose = require('mongoose');
 // const routes = require('./routes/messages')
 const express = require('express');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
 const app = express();
-const socket = require('socket.io');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 app.use(express.json());
-
 // app.use(routes)
 
-const server = app.listen(3000, () => {
-    console.log('listening on 3000...');
-})
+const httpServer = createServer(app);
+const io = new Server(httpServer);
 
-const io = socket(server)
-io.on("connection", (socket) => {
+io.on("connection", (sokt) => {
     console.log('new connection');
-    socket.on('chat-message', (message) => {
-        console.log(message);
+    sokt.on('chat-message', (message) => {
+        sokt.broadcast.emit('message', message);
     })
 })
+
+console.log('listening on port 3000...');
+httpServer.listen(3000);
--- public/script.js
+++ public/script.js
@@ -1,10 +1,21 @@
 const socket = io()
 const msgForm = document.getElementById('send-container')
+const msgContainer = document.getElementById('container')
 const msgInput = document.getElementById('msg-input')
+
+socket.on('message', (data) => {
+    appendMessage(data);
+})
 
 msgForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const message = msgInput.value
-    socket.emit('chat-message', message)
-    msgInput.value = ''
+    socket.emit('chat-message', message);
+    msgInput.value = '';
 })
+
+function appendMessage(msg) {
+    const msgElement = document.createElement('div');
+    msgElement.innerText = msg;
+    msgContainer.appendChild(msgElement);
+}
