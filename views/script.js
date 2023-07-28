9d81463e6d2aa6d090fbd31878652083cfbf5aef
Shivam <shivammadlani5@gmail.com>
2023-07-28 14:13:56 +0530

    added chat bubble with sender initials added sender name as chat header

--- views/script.js
+++ views/script.js
@@ -24,9 +24,22 @@ function appendMessage(sender, text) {
         msgElement.classList.add("chat", "chat-end");
     else
         msgElement.classList.add("chat", "chat-start");
+    const avatarDiv = document.createElement("div");
+    avatarDiv.classList.add("chat-image", "avatar", "placeholder");
+    const senderInitials = document.createElement("div");
+    senderInitials.classList.add("bg-neutral-focus", "text-neutral-content", "rounded-full", "w-12");
+    senderInitials.innerText = sender[0].toUpperCase();
+    avatarDiv.appendChild(senderInitials);
+    msgElement.appendChild(avatarDiv);
     const msgData = document.createElement("div");
     msgData.classList.add("chat-bubble");
-    msgData.innerText = sender + ": " + text;
+    msgData.innerText = text;
+
+    const msgSender = document.createElement("div");
+    msgSender.innerText = sender;
+    msgSender.classList.add("chat-header");
+    msgElement.appendChild(msgSender);
+
     msgElement.appendChild(msgData);
     msgContainer.appendChild(msgElement);
     msgContainer.scrollTop = msgContainer.scrollHeight;
